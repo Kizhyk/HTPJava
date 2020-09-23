6c970aa17315d7360925c3d405edbe62046d9198
Kizhyk <nik.kizhyk@gmail.com>
2020-09-23 21:06:56 +0300

    task: complete SwitchExample in task5
    (cherry picked from commit baf81029321b9883da88546f10232c8f71256b90)

--- HomeWork/src/com/kizhyk/task5/SwitchExample.java
+++ HomeWork/src/com/kizhyk/task5/SwitchExample.java
@@ -1,5 +1,30 @@
 package com.kizhyk.task5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SwitchExample {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+
+        switch (name) {
+            case "Вася":
+                System.out.print("Привет!\nЯ так долго тебя ждал!");
+                break;
+
+            case "Анастасия":
+                System.out.print("Я тебя так долго ждал!");
+                break;
+
+            default:
+                System.out.print("Добрый день, а вы кто?");
+                break;
+        }
+
+        reader.close();
+    }
 
 }
