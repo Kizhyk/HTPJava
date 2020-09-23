b86620412dea0937931d73c1e3b4f6d13949fcad
Kizhyk <nik.kizhyk@gmail.com>
2020-09-23 21:32:05 +0300

    task: complete task4
    (cherry picked from commit e9d49134ae4c31d978acecd73bc1e1f262c5c500)

--- HomeWork/src/com/kizhyk/Task4.java
+++ HomeWork/src/com/kizhyk/Task4.java
@@ -2,4 +2,24 @@ package com.kizhyk;
 
 public class Task4 {
 
+    public static void main(String[] args) {
+
+        if (sleepIn(false, true)) {
+            System.out.println("... (Nothing should wake a man)");
+        } else {
+            System.out.println("Wake up time!");
+        }
+
+    }
+
+    private static boolean sleepIn(boolean weekday, boolean vacation) {
+        boolean isSleepTime = false;
+
+        if (weekday || vacation) {
+            isSleepTime = true;
+        }
+
+        return isSleepTime;
+    }
+
 }
