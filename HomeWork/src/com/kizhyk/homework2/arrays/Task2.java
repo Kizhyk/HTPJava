501b334f2c2b705ac4801dd69080eee73973c792
Kizhyk <nik.kizhyk@gmail.com>
2020-10-04 00:27:11 +0300

    task: complete task2 in arrays

--- HomeWork/src/com/kizhyk/homework2/arrays/Task2.java
+++ HomeWork/src/com/kizhyk/homework2/arrays/Task2.java
@@ -2,4 +2,11 @@ package com.kizhyk.homework2.arrays;
 
 public class Task2 {
 
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 1; i < array.length; i += 2) {
+            System.out.println(array[i]);
+        }
+    }
 }
