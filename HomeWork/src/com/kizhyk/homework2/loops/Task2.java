aae1ddc9efc1c27bb7bb6ce3e825236dbdc99af7
Kizhyk <nik.kizhyk@gmail.com>
2020-10-03 11:53:49 +0300

    task: complete task2 in loops
    (cherry picked from commit 7d2f8ab5b7c3bef7264a63e11b2cc88f27eac064)

--- HomeWork/src/com/kizhyk/homework2/loops/Task2.java
+++ HomeWork/src/com/kizhyk/homework2/loops/Task2.java
@@ -1,5 +1,25 @@
 package com.kizhyk.homework2.loops;
 
+import java.util.Arrays;
+
 public class Task2 {
 
+    public static void main(String[] args) {
+
+        try {
+            int[] numbers = Arrays.stream(args[0].split("")).mapToInt(Integer::parseInt).toArray();
+            int composition = 1;
+
+            for (int i = 1; i < numbers.length; i++) {
+                composition *= numbers[i - 1];
+                System.out.printf("%d * ", numbers[i - 1]);
+            }
+
+            System.out.printf("%d = %d", numbers[numbers.length - 1], composition);
+        } catch (ArrayIndexOutOfBoundsException arrayException) {
+            System.out.println("Insert program arguments");
+        } catch (NumberFormatException numberException) {
+            System.out.println("Argument should be a not-float number");
+        }
+    }
 }
