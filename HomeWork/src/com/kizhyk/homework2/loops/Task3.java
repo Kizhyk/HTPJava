c72b9aa02851890425aecae5b1fbb07eaf2e21f0
Kizhyk <nik.kizhyk@gmail.com>
2020-10-03 21:00:06 +0300

    task: complete task3 in loops

--- HomeWork/src/com/kizhyk/homework2/loops/Task3.java
+++ HomeWork/src/com/kizhyk/homework2/loops/Task3.java
@@ -1,5 +1,36 @@
 package com.kizhyk.homework2.loops;
 
+import java.util.Scanner;
+
 public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            double base = sc.nextDouble();
+            int power = sc.nextInt();
+
+            System.out.println(elevate(base, power));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static double elevate(double base, int power) {
+        if (power < 0) {
+            throw new IllegalArgumentException();
+        } else if (power == 0) {
+            return 1;
+        }
+
+        double result = 1;
+
+        for (int i = 0; i < power; i++) {
+            result *= base;
+        }
+
+        return result;
+    }
 
 }
