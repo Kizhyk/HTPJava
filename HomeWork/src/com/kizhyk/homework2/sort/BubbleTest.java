e058816f110acd4be81a569656a786c64a075181
Kizhyk <nik.kizhyk@gmail.com>
2020-10-04 22:00:20 +0300

    task: complete BubbleTest class in sort

--- HomeWork/src/com/kizhyk/homework2/sort/BubbleTest.java
+++ HomeWork/src/com/kizhyk/homework2/sort/BubbleTest.java
@@ -1,2 +1,19 @@
-package com.kizhyk.homework2.sort;public class BubbleTest {
+package com.kizhyk.homework2.sort;
+
+import java.util.Arrays;
+
+public class BubbleTest extends Test {
+
+    public static void main(String[] args) {
+        printChanges(array1);
+        printChanges(array2);
+        printChanges(array3);
+        printChanges(array4);
+    }
+
+    public static void printChanges(int[] array) {
+        System.out.print(Arrays.toString(array) + " -> ");
+        Sorting.bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+    }
 }
