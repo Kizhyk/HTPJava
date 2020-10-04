60db690cd6f8979418235b2133c149f6ad3e2e02
Kizhyk <nik.kizhyk@gmail.com>
2020-10-04 17:18:23 +0300

    task: complete bubble sorting in Sorting in sort
    (cherry picked from commit 80f43c501ee9ad2424038b074a643ace7b212afd)

--- HomeWork/src/com/kizhyk/homework2/sort/Sorting.java
+++ HomeWork/src/com/kizhyk/homework2/sort/Sorting.java
@@ -2,4 +2,34 @@ package com.kizhyk.homework2.sort;
 
 public class Sorting {
 
+    private Sorting() {}
+
+    /*public static void bubbleSort(int[] array) {        //первая попытка
+        for (int i = 0; i < array.length - 1; i++) {      //не знаю, что за алгоритм получился,
+            for (int j = i + 1; j < array.length; j++) {  //но он работает, пишется легче
+                if (array[j] < array[i]) {                //чем пузырьковая и имеет такую
+                    int tmp = array[i];                   //же сложность (даже меньше).
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }*/
+
+    public static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+
+        while (!isSorted) {
+            isSorted = true;
+
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    isSorted = false;
+                    int tmp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = tmp;
+                }
+            }
+        }
+    }
 }
