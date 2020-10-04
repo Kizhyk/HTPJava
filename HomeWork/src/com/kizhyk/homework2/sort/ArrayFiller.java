8988fd0b8941b9449fb0089253914f994be6ceb3
Kizhyk <nik.kizhyk@gmail.com>
2020-10-04 22:12:26 +0300

    task: complete ArrayFiller class in sort
    (cherry picked from commit e332f4c38ad6d920da8ffda8a4f2a811b04dd851)

--- HomeWork/src/com/kizhyk/homework2/sort/ArrayFiller.java
+++ HomeWork/src/com/kizhyk/homework2/sort/ArrayFiller.java
@@ -1,5 +1,30 @@
 package com.kizhyk.homework2.sort;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ArrayFiller {
-    
+    private static int[] createRandomArray(int sizeCeil, int elementCeil) {
+        int[] array = new int[(int) (Math.random() * sizeCeil)];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * elementCeil);
+        }
+
+        return array;
+    }
+
+    private static int[] createConsoleArray() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please insert size:");
+
+        int[] array = new int[Integer.parseInt(reader.readLine())];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        return array;
+    }
 }
