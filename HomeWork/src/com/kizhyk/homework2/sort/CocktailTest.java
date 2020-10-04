7892570b82ec2b6f4a6617e73631edecfbcea940
Kizhyk <nik.kizhyk@gmail.com>
2020-10-04 22:01:53 +0300

    task: complete CocktailTest class in sort
    (cherry picked from commit 5935f51d05ef8bc5860287da945559cae4afc708)

--- HomeWork/src/com/kizhyk/homework2/sort/CocktailTest.java
+++ HomeWork/src/com/kizhyk/homework2/sort/CocktailTest.java
@@ -1,5 +1,19 @@
 package com.kizhyk.homework2.sort;
 
+import java.util.Arrays;
+
 public class CocktailTest extends Test {
 
+    public static void main(String[] args) {
+        printChanges(array1);
+        printChanges(array2);
+        printChanges(array3);
+        printChanges(array4);
+    }
+
+    public static void printChanges(int[] array) {
+        System.out.print(Arrays.toString(array) + " -> ");
+        Sorting.cocktailSort(array);
+        System.out.println(Arrays.toString(array));
+    }
 }
