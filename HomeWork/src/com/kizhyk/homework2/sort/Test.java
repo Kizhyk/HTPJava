793241180abca83e7b5d6b096d5e6bc36c535396
Kizhyk <nik.kizhyk@gmail.com>
2020-10-04 21:59:56 +0300

    task: complete Test class in sort
    (cherry picked from commit 7e2965c62f06ce508ba3f24da53ab83603a902e2)

--- HomeWork/src/com/kizhyk/homework2/sort/Test.java
+++ HomeWork/src/com/kizhyk/homework2/sort/Test.java
@@ -1,5 +1,11 @@
 package com.kizhyk.homework2.sort;
 
 public abstract class Test {
+    static int[] array1 = {1,2,3,4,5,6};
+    static int[] array2 = {1,1,1,1};
+    static int[] array3 = {9,1,5,99,9,9};
+    static int[] array4 = {};
+
+    public static void printChanges(int[] array) {}
 
 }
