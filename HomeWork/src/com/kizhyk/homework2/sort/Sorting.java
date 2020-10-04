ed079dee1166d3d5eb447ca7ea4cf3a9203e4f3c
Kizhyk <nik.kizhyk@gmail.com>
2020-10-04 21:02:42 +0300

    task: complete cocktail sorting in Sorting in sort
    (cherry picked from commit 039c8617360daff2483cf79f9fac40136a10e31a)

--- HomeWork/src/com/kizhyk/homework2/sort/Sorting.java
+++ HomeWork/src/com/kizhyk/homework2/sort/Sorting.java
@@ -32,4 +32,33 @@ public class Sorting {
             }
         }
     }
+
+    public static void cocktailSort(int[] array) {
+        boolean isSorted = false;
+        int count = array.length - 1;
+
+        while (!isSorted) {
+            isSorted = true;
+
+            for (int i = 0; i < count; i++) {
+                if (array[i] > array[i + 1]) {
+                    isSorted = false;
+                    int tmp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = tmp;
+                }
+            }
+
+            for (int i = count - 1; i > 0; i--) {
+                if (array[i] < array[i - 1]) {
+                    isSorted = false;
+                    int tmp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = tmp;
+                }
+            }
+
+            count--;
+        }
+    }
 }
