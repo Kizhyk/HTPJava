eaea6a7f048b33af4fa64dee7ab788e4458ac558
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 13:38:45 +0300

    task: complete CalculatorWithOperator class in task2

--- HomeWork/src/com/kizhyk/homework3/task2/CalculatorWithOperator.java
+++ HomeWork/src/com/kizhyk/homework3/task2/CalculatorWithOperator.java
@@ -1,2 +1,35 @@
-package com.kizhyk.homework3.task2;public class CalculatorWithOperator {
+package com.kizhyk.homework3.task2;
+
+public class CalculatorWithOperator {
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double sqrt(double a) {
+        return Math.sqrt(a);
+    } //ряды мне тут расписывать как-то лениво
+
+    public double abs(double a) {
+        return (a < 0) ? -a : a;
+    }
+
+    public double pow(double a, int b) {
+        for (int i = 0; i < abs(b) - 1; i++) {
+            a *= a;
+        }
+        return (b < 0) ? divide(1, a) : a;
+    }
 }
