e8196812f0b65b1a7752b11f1b3203e83335cae3
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 21:06:16 +0300

    task: complete ICalculator interface in task6

--- HomeWork/src/com/kizhyk/homework3/task6/ICalculator.java
+++ HomeWork/src/com/kizhyk/homework3/task6/ICalculator.java
@@ -2,4 +2,17 @@ package com.kizhyk.homework3.task6;
 
 public interface ICalculator {
 
+    double sum(double a, double b);
+
+    double subtract(double a, double b);
+
+    double multiply(double a, double b);
+
+    double divide(double a, double b);
+
+    double sqrt(double a);
+
+    double abs(double a);
+
+    double pow(double a, int b);
 }
