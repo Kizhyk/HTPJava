c77f7de5e3f0669e75693f1bbb95a3ac26eea30f
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 13:49:37 +0300

    task: complete CalculatorWithMathCopyMain class in task3

--- HomeWork/src/com/kizhyk/homework3/task3/CalculatorWithMathCopyMain.java
+++ HomeWork/src/com/kizhyk/homework3/task3/CalculatorWithMathCopyMain.java
@@ -1,4 +1,10 @@
 package com.kizhyk.homework3.task3;
 
 public class CalculatorWithMathCopyMain {
+    public static void main(String[] args) {
+        CalculatorWithMathCopy calc = new CalculatorWithMathCopy();
+
+        double result = calc.sum(4.1, calc.sum(calc.multiply(15, 7), calc.pow(calc.divide(28, 5), 2)));
+        System.out.println(result);
+    }
 }
