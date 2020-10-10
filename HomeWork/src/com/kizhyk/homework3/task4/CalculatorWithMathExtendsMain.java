4afa536bccdb74c64d33f14b6882bfa349dbf06a
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 14:10:41 +0300

    task: complete CalculatorWithMathExtendsMain class in task4

--- HomeWork/src/com/kizhyk/homework3/task4/CalculatorWithMathExtendsMain.java
+++ HomeWork/src/com/kizhyk/homework3/task4/CalculatorWithMathExtendsMain.java
@@ -1,4 +1,10 @@
 package com.kizhyk.homework3.task4;
 
 public class CalculatorWithMathExtendsMain {
+    public static void main(String[] args) {
+        CalculatorWithMathExtends calc = new CalculatorWithMathExtends();
+
+        double result = calc.sum(4.1, calc.sum(calc.multiply(15, 7), calc.pow(calc.divide(28, 5), 2)));
+        System.out.println(result);
+    }
 }
