fe6ff0f3789c1415bf2a48a25587b1bb6f26dc9f
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 21:24:28 +0300

    task: complete CalculatorWithCounterInterfaceMain class in task6

--- HomeWork/src/com/kizhyk/homework3/task6/CalculatorWithCounterInterfaceMain.java
+++ HomeWork/src/com/kizhyk/homework3/task6/CalculatorWithCounterInterfaceMain.java
@@ -1,5 +1,12 @@
 package com.kizhyk.homework3.task6;
 
+import com.kizhyk.homework3.task4.CalculatorWithMathExtends;
+
 public class CalculatorWithCounterInterfaceMain {
-    
+    public static void main(String[] args) {
+        CalculatorWithCounterInterface calc = new CalculatorWithCounterInterface(new CalculatorWithMathExtends());
+
+        double result = calc.sum(4.1, calc.sum(calc.multiply(15, 7), calc.pow(calc.divide(28, 5), 2)));
+        System.out.println(result);
+    }
 }
