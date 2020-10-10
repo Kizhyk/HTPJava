10bbc9c3ed9cac8d0bc0abd9f79dd2b7180185dc
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 12:59:20 +0300

    task: complete WithOutCalculatorMain class in task1

--- HomeWork/src/com/kizhyk/homework3/task1/WithOutCalculatorMain.java
+++ HomeWork/src/com/kizhyk/homework3/task1/WithOutCalculatorMain.java
@@ -1,5 +1,7 @@
 package com.kizhyk.homework3.task1;
 
 public class WithOutCalculatorMain {
-    
+    public static void main(String[] args) {
+        System.out.println(4.1 + 15 * 7 + ((double) 28 / 5) * ((double) 28 / 5));
+    }
 }
