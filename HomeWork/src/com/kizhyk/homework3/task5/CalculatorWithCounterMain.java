1e0f88384cf103a880816f426828651322554cff
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 20:58:13 +0300

    task: complete CalculatorWithCounterMain class in task5

--- HomeWork/src/com/kizhyk/homework3/task5/CalculatorWithCounterMain.java
+++ HomeWork/src/com/kizhyk/homework3/task5/CalculatorWithCounterMain.java
@@ -1,5 +1,12 @@
 package com.kizhyk.homework3.task5;
 
+import com.kizhyk.homework3.task4.CalculatorWithMathExtends;
+
 public class CalculatorWithCounterMain {
-    
+    public static void main(String[] args) {
+        CalculatorWithCounter calc = new CalculatorWithCounter(new CalculatorWithMathExtends());
+
+        double result = calc.sum(4.1, calc.sum(calc.multiply(15, 7), calc.pow(calc.divide(28, 5), 2)));
+        System.out.println(result);
+    }
 }
