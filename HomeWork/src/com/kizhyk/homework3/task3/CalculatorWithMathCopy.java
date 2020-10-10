e1e050c71301fbd634e15695da712f9120446f36
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 13:47:07 +0300

    task: complete CalculatorWithMathCopy class in task3

--- HomeWork/src/com/kizhyk/homework3/task3/CalculatorWithMathCopy.java
+++ HomeWork/src/com/kizhyk/homework3/task3/CalculatorWithMathCopy.java
@@ -1,4 +1,32 @@
 package com.kizhyk.homework3.task3;
 
 public class CalculatorWithMathCopy {
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    public double abs(double a) {
+        return Math.abs(a);
+    }
+
+    public double pow(double a, int b) {
+        return Math.pow(a, b);
+    }
 }
