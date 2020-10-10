75f22df4c1058561ee156857d33a9b7e503bc901
Kizhyk <nik.kizhyk@gmail.com>
2020-10-11 00:06:35 +0300

    task: complete CalculatorWithMemoryMain class in task7
    (cherry picked from commit bcc1927b77794d69e1d882e27facb38b794a0da9)

--- HomeWork/src/com/kizhyk/homework3/task7/CalculatorWithMemoryMain.java
+++ HomeWork/src/com/kizhyk/homework3/task7/CalculatorWithMemoryMain.java
@@ -1,5 +1,12 @@
 package com.kizhyk.homework3.task7;
 
+import com.kizhyk.homework3.task4.CalculatorWithMathExtends;
+
 public class CalculatorWithMemoryMain {
-    
+    public static void main(String[] args) {
+        CalculatorWithMemory calc = new CalculatorWithMemory(new CalculatorWithMathExtends());
+
+        System.out.println(calc.sum(4.1, calc.sum(calc.multiply(15, 7),
+                calc.pow(calc.divide(28, 5), 2)))); //ладно, не очень-то и хотелось...
+    }
 }
