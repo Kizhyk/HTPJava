a5f62c0ec002b2d327227d54d329ad7c9b9c7d67
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 13:35:30 +0300

    task: complete CalculatorWithOperatorMain class in task2

--- HomeWork/src/com/kizhyk/homework3/task2/CalculatorWithOperatorMain.java
+++ HomeWork/src/com/kizhyk/homework3/task2/CalculatorWithOperatorMain.java
@@ -1,4 +1,10 @@
 package com.kizhyk.homework3.task2;
 
 public class CalculatorWithOperatorMain {
+    public static void main(String[] args) {
+        CalculatorWithOperator calc = new CalculatorWithOperator();
+
+        double result = calc.sum(4.1, calc.sum(calc.multiply(15, 7), calc.pow(calc.divide(28, 5), 2)));
+        System.out.println(result);
+    }
 }
