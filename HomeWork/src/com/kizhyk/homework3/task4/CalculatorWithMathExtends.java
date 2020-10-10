174a4c3f05c36740db31caf7d2e0e20a3e48a35a
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 14:04:03 +0300

    task: complete CalculatorWithMathExtends class in task4

--- HomeWork/src/com/kizhyk/homework3/task4/CalculatorWithMathExtends.java
+++ HomeWork/src/com/kizhyk/homework3/task4/CalculatorWithMathExtends.java
@@ -1,4 +1,25 @@
 package com.kizhyk.homework3.task4;
 
-public class CalculatorWithMathExtends {
+import com.kizhyk.homework3.task2.CalculatorWithOperator;
+
+public class CalculatorWithMathExtends extends CalculatorWithOperator {
+
+    @Override
+    public double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    @Override
+    public double abs(double a) {
+        return Math.abs(a);
+    }
+
+    @Override
+    public double pow(double a, int b) {
+        if (a < 0) {
+            throw new IllegalArgumentException();
+        }
+        return Math.pow(a, b);
+    }
+
 }
