e356519f0403500be1600015d2008a6d187dd9c6
Kizhyk <nik.kizhyk@gmail.com>
2020-10-10 21:23:08 +0300

    task: complete CalculatorWithCounterInterface class in task6
    (cherry picked from commit 1c6697e5e38dc86fb576d500bace096dc982db78)

--- HomeWork/src/com/kizhyk/homework3/task6/CalculatorWithCounterInterface.java
+++ HomeWork/src/com/kizhyk/homework3/task6/CalculatorWithCounterInterface.java
@@ -1,5 +1,50 @@
 package com.kizhyk.homework3.task6;
 
-public class CalculatorWithCounterInterface {
-    
+public class CalculatorWithCounterInterface implements ICalculator {
+    private final ICalculator calc;
+
+    private long count = 0;
+
+    public CalculatorWithCounterInterface(ICalculator calc) {
+        this.calc = calc;
+    }
+
+    public double sum(double a, double b) {
+        count++;
+        return calc.sum(a, b);
+    }
+
+    public double subtract(double a, double b) {
+        count++;
+        return calc.subtract(a, b);
+    }
+
+    public double multiply(double a, double b) {
+        count++;
+        return calc.multiply(a, b);
+    }
+
+    public double divide(double a, double b) {
+        count++;
+        return calc.divide(a, b);
+    }
+
+    public double sqrt(double a) {
+        count++;
+        return calc.sqrt(a);
+    }
+
+    public double abs(double a) {
+        count++;
+        return calc.abs(a);
+    }
+
+    public double pow(double a, int b) {
+        count++;
+        return calc.pow(a, b);
+    }
+
+    public long getCountOperation() {
+        return count;
+    }
 }
