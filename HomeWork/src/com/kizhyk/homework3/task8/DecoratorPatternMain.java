faa4d0ceab6fbbc25bff30432dde78625fb2c5e7
Kizhyk <nik.kizhyk@gmail.com>
2020-10-12 15:13:14 +0300

    task: complete DecoratorPatternMain class in task8

--- HomeWork/src/com/kizhyk/homework3/task8/DecoratorPatternMain.java
+++ HomeWork/src/com/kizhyk/homework3/task8/DecoratorPatternMain.java
@@ -1,5 +1,26 @@
 package com.kizhyk.homework3.task8;
 
+import com.kizhyk.homework3.task4.CalculatorWithMathExtends;
+import com.kizhyk.homework3.task6.CalculatorWithCounterInterface;
+import com.kizhyk.homework3.task6.ICalculator;
+import com.kizhyk.homework3.task7.CalculatorWithMemory;
+
 public class DecoratorPatternMain {
-    
+    public static void main(String[] args) {
+        ICalculator calc = new CalculatorWithCounterInterface(new CalculatorWithMemory(new CalculatorWithMathExtends()));
+
+        double result = calc.sum(4.1, calc.sum(calc.multiply(15, 7), calc.pow(calc.divide(28, 5), 2)));
+        System.out.println(result);
+
+        if (calc instanceof CalculatorWithCounterInterface) {
+            CalculatorWithCounterInterface extendedCalc = (CalculatorWithCounterInterface) calc;
+            System.out.println(extendedCalc.getCountOperation());
+
+            if (extendedCalc.getCalculator() instanceof CalculatorWithMemory) {
+                CalculatorWithMemory extendedCalc2 = (CalculatorWithMemory) extendedCalc.getCalculator();
+                extendedCalc2.writeMemory();
+                System.out.println(extendedCalc2.getMemory());
+            }
+        }
+    }
 }
