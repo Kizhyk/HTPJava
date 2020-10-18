d7ae047651fa3d6b6ecd9bee6e8cd3256b414b04
Kizhyk <nik.kizhyk@gmail.com>
2020-10-19 00:13:39 +0300

    task: add constructors in DataContainer

--- HomeWork/src/com/kizhyk/homework4/DataContainer.java
+++ HomeWork/src/com/kizhyk/homework4/DataContainer.java
@@ -1,5 +1,16 @@
 package com.kizhyk.homework4;
 
-public class DataContainer <T> {
+import java.lang.reflect.Array;
 
+public class DataContainer<T> {
+    private static final int DEFAULT_SIZE = 0;
+    private T[] data;
+
+    public DataContainer(Class<T> type, int size) {
+        data = (T[]) Array.newInstance(type, size);
+    }
+
+    public DataContainer(Class<T> type) {
+        this(type, DEFAULT_SIZE);
+    }
 }
