4eb73463d1e3d7fc0bfc14c9a83b8fa17948a571
Kizhyk <nik.kizhyk@gmail.com>
2020-10-19 00:15:00 +0300

    task: add method grow() to DataContainer

--- HomeWork/src/com/kizhyk/homework4/DataContainer.java
+++ HomeWork/src/com/kizhyk/homework4/DataContainer.java
@@ -1,9 +1,11 @@
 package com.kizhyk.homework4;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class DataContainer<T> {
     private static final int DEFAULT_SIZE = 0;
+    private static final int GROW_SIZE = 1;
     private T[] data;
 
     public DataContainer(Class<T> type, int size) {
@@ -12,5 +14,9 @@ public class DataContainer<T> {
 
     public DataContainer(Class<T> type) {
         this(type, DEFAULT_SIZE);
+    }
+
+    private void grow() {
+        data = Arrays.copyOf(data, data.length + GROW_SIZE);
     }
 }
