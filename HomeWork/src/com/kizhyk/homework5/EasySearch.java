f3f069994ed60eb90875e2ca1db17c969138dbee
Kizhyk <nik.kizhyk@gmail.com>
2020-11-04 20:05:25 +0300

    task: complete EasySearch class

--- HomeWork/src/com/kizhyk/homework5/EasySearch.java
+++ HomeWork/src/com/kizhyk/homework5/EasySearch.java
@@ -1,5 +1,19 @@
 package com.kizhyk.homework5;
 
 public class EasySearch implements ISearchEngine {
+    @Override
+    public long search(String text, String word) {
+        text = Librarian.cleanText(text);
+        word = String.format(" %s ", word);
 
+        int i = 0;
+        long count = 0;
+
+        while ((i = text.indexOf(word, i)) != -1) {
+            i++;
+            count++;
+        }
+
+        return count;
+    }
 }
